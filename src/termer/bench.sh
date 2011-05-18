0c6a8206e807ec8c5fcb7bd069bef56520ef1481
Richard Uhler <ruhler@csail.mit.edu>
2011-05-17 21:27:25 -0400

    Recorded output of top for performance evaluation, other performance thoughts.

--- src/termer/bench.sh
+++ src/termer/bench.sh
@@ -1,13 +1,3 @@
 
-sh
-
-set -v on
-
-echo "Hello! How are you?"
-echo "Is this enough text to test the performance of the terminal"
-echo "How many more lines do I need?"
-
-echo "I think just a few more would be great"
-echo "Then I'll have enough to make a good time measurement!"
-
+cat src/termer/toptrace
 
